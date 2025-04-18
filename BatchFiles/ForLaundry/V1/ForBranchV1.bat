f292745c8ff2cf1d1b829a12f8305f65e16f5e7c
keshavsoft <nknnkumar@yahoo.com>
2025-04-18 16:28:26 +0530

    batch files

--- BatchFiles/ForLaundry/V1/ForBranchV1.bat
+++ BatchFiles/ForLaundry/V1/ForBranchV1.bat
@@ -1,9 +1,9 @@
 @echo off
-cd ..\LaundryFrontEndForBranch
+cd ..\LaundryFEBranchV1
 git pull
 
 call npm run Today-Orders
-call npm run Today-QrCodes
+call npm run Today-QrCodesK
 call npm run NewOrders
 call npm run CustomersToOrders
 call npm run Masters
@@ -13,17 +13,7 @@ call npm run TodayReports
 call npm run Dashboard-QrCodeWise-AsArray
 call npm run Branch-CommonConfig-Menu
 call npm run Dashboard-CommonConfig-AsArray
-call npm run Factory-ToFactory
-call npm run FromFactory-Completion
-call npm run FromFactory-EntryReturn
-call npm run FromFactory-WashingReturn
-call npm run FromFactory-PressingReturn
-call npm run FromFactory-EntryReturnQrCodes
-call npm run FromFactory-WashingReturnQrCodes
-call npm run FromFactory-PressingReturnQrCodes
-call npm run FromFactory-CompletionQrCodes
 call npm run Dashboard-QrCodeAtStage-BranchFilter
-call npm run Delivery
 call npm run Orders
 call npm run Payments
 call npm run QrCodes
