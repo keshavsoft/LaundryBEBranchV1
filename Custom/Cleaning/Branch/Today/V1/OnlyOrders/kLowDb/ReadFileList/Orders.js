50c90af194a7ea83c65f690eed6bb65c5dde63f1
Srivasu215 <sri.vasu215@gmail.com>
2025-04-22 19:47:55 +0530

    is IsQrCodesRaised add

--- Custom/Cleaning/Branch/Today/V1/OnlyOrders/kLowDb/ReadFileList/Orders.js
+++ Custom/Cleaning/Branch/Today/V1/OnlyOrders/kLowDb/ReadFileList/Orders.js
@@ -1,11 +1,13 @@
 import { StartFunc as StartFuncCommonFuncs } from '../CommonFuncs/Transactions.js';
+import { StartFunc as StartFuncQrCodes } from '../../../../../../../../binV4/QrCodes/CommonPull/kLowDb/PullData/returnAsArray.js';
 
 let StartFunc = ({ inBranch }) => {
     let LocalFindValue = new Date().toLocaleDateString('en-GB').replace(/\//g, '/');
+    const modifiedBranch = inBranch.replace("BranOrders", "");
 
     let LocalBranchName = inBranch;
     let LocalReturnData = { KTF: false }
-
+    const Qrdb = StartFuncQrCodes();
     const Orderdb = StartFuncCommonFuncs({ inBranchName: LocalBranchName });
     Orderdb.read();
 
@@ -17,10 +19,38 @@ let StartFunc = ({ inBranch }) => {
         LocalReturnData.KReason = "No Data"
         return LocalReturnData;
     };
+    let LocalInsertAggValues = jFLocalInsertQrCodeData({ 
+        inBranchName: modifiedBranch, 
+        inOrderData: LocalFilterBranchData, 
+        inQrCodeData: Qrdb 
+    });
     LocalReturnData.KTF = true;
     LocalReturnData.JsonData = LocalFilterBranchData.slice().reverse();
 
     return LocalReturnData;
+};
+
+let jFLocalInsertQrCodeData = ({ inBranchName, inOrderData, inQrCodeData }) => {
+    let LocalBranchName = inBranchName;
+    let jVarLocalReturnArray = [];
+
+    inOrderData.forEach(element => {
+        element.IsQrCodesRaised = false;
+        element.TotalItems = 0;
+        
+        if (Array.isArray(inQrCodeData)) {
+            let FilterCheck = inQrCodeData.filter(ele => 
+                ele.OrderNumber == element.pk && ele.BookingData.OrderData.BranchName == LocalBranchName
+            );
+            if (FilterCheck.length > 0) {
+                element.TotalItems = FilterCheck.length;
+                element.IsQrCodesRaised = true;
+            }
+        }
+        jVarLocalReturnArray.push(element);
+    });
+
+    return jVarLocalReturnArray;
 };
 
 export { StartFunc };
